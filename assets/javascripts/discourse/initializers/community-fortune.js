19c01e8010e666535baa15781225d477868c20b4
jonggil.kim <jonggil.kim@dkbmc.com>
2025-08-05 13:36:28 +0900

    코드수정

--- assets/javascripts/discourse/initializers/community-fortune.js
+++ assets/javascripts/discourse/initializers/community-fortune.js
@@ -4,18 +4,13 @@ export default {
   name: "community-fortune",
 
   initialize() {
-    withPluginApi("0.8.13", (api) => {
+    withPluginApi("1.17.0", (api) => {
       const siteSettings = api.container.lookup("service:site-settings");
       if (!siteSettings.community_fortune_enabled) {
         return;
       }
 
-      // welcome-banner-below-input 위치에 컴포넌트 렌더링
-      api.decoratePluginOutlet("welcome-banner-below-input", (content) => {
-        return {
-          component: "community-fortune",
-        };
-      });
+      api.renderInOutlet("welcome-banner-below-input", "community-fortune");
     });
   },
 };
